60bbfb0602b4400de9fbc13c640de9ae920f49be
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-06 17:26:03 +0200

    Removed testing code and made into a module

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
+import { CheckBooleanProps, CheckStringProps, CheckNumberProps, checkBoolean, checkNumber, checkString} from "./bigChecks";
+import { chainChecks, chainChecksMiddleware } from "./check";
 import SmallCheck from "./smallCheck";
-import { chainBigChecks, chainSmallChecks } from "./check";
-import { checkNumber } from "./bigChecks";
+import { CheckFunction, CheckReturn, ErrorFunction, JsonObject, Middleware, SuccessFunction, VerySmallCheckFunction } from "./types";
 
-function errorFunction(errorCode: number, errorMsg: string) {
-    console.log(errorCode, errorMsg);
+export default {
+    bigChecks: {
+        checkBoolean,
+        checkNumber,
+        checkString
+    },
+    smallChecks: SmallCheck,
+    chain: {
+        chainChecks,
+        chainChecksMiddleware
+    }
 }
 
-function successFunction(successCode: number, successMsg: string) {
-    console.log(successCode, successMsg);
+export {
+    CheckReturn,
+    ErrorFunction,
+    SuccessFunction,
+    JsonObject,
+
+    VerySmallCheckFunction,
+
+    Middleware,
+
+    CheckFunction
 }
 
-chainSmallChecks(errorFunction, successFunction, {"test": 1},
-    new SmallCheck("test").
-    isBoolean(-1, "Value is not a boolean."),
-    new SmallCheck("test2").
-    isNaN(-1, "")
-);
-
-chainBigChecks(errorFunction, successFunction, {"test": 1, "test2": 0}, 
-    checkNumber({errorCode:0, errorMsg:"", nameOfJsonAttribute:"test", successCode:0, successMsg:""}),
-    checkNumber({errorCode:0, errorMsg:"", nameOfJsonAttribute:"test2", successCode:0, successMsg:""})
-)();
+export {
+    CheckBooleanProps,
+    CheckNumberProps,
+    CheckStringProps
+}
