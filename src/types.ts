43615508f75206792a640931148631ba6399f3d3
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-06 17:24:46 +0200

    Renamed and added some tpes

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,14 @@ type ErrorFunction = (errorCode: number, errorMsg: string) => void;
 type SuccessFunction = (successCode: number, successMsg: string) => void;
 type JsonObject = { [key: string]: any; };
 
-type SmallCheckFunction = (jsonObject: { [key: string]: any }) => CheckReturn;
+// type SmallCheckFunction = (jsonObject: { [key: string]: any }) => CheckReturn;
 type VerySmallCheckFunction = (toCheck: any) => boolean;
 
-type BigCheckFunction = (jsonObject: JsonObject, successFunction: SuccessFunction, errorFunction: ErrorFunction) => CheckReturn;
+// type BigCheckFunction = (jsonObject: JsonObject, successFunction: SuccessFunction, errorFunction: ErrorFunction) => CheckReturn;
 
 type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+type CheckFunction = (jsonObject: JsonObject, errorFunction: ErrorFunction, successFunction: SuccessFunction) => CheckReturn;
 
 export {
     CheckReturn,
@@ -18,10 +20,9 @@ export {
     SuccessFunction,
     JsonObject,
 
-    SmallCheckFunction,
     VerySmallCheckFunction,
 
-    BigCheckFunction,
+    Middleware,
 
-    Middleware
+    CheckFunction
 }
