ad7a7286643849394f7e62979d8b1cbf84cbfb00
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-07 13:25:28 +0200

    Added and changed some types
    Changed "ErrorFunction" and "SuccessFunction" to also require the key.
    Added types  for the modified ExpressJS request.

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 type CheckReturn = [boolean, number, string];
-type ErrorFunction = (errorCode: number, errorMsg: string) => void;
-type SuccessFunction = (successCode: number, successMsg: string) => void;
+type ErrorFunction = (errorCode: number, errorMsg: string, key: string) => void;
+type SuccessFunction = (successCode: number, successMsg: string, key: string) => void;
 type JsonObject = { [key: string]: any; };
 
 // type SmallCheckFunction = (jsonObject: { [key: string]: any }) => CheckReturn;
@@ -10,9 +10,25 @@ type VerySmallCheckFunction = (toCheck: any) => boolean;
 
 // type BigCheckFunction = (jsonObject: JsonObject, successFunction: SuccessFunction, errorFunction: ErrorFunction) => CheckReturn;
 
-type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+type Middleware = (req: CheckedRequest, res: Response, next: NextFunction) => void;
 
 type CheckFunction = (jsonObject: JsonObject, errorFunction: ErrorFunction, successFunction: SuccessFunction) => CheckReturn;
+
+type CheckedRequestEntry = {
+    error: boolean;
+    separateChecks: {
+        [key: string]: {
+            error: boolean;
+            code: number;
+            msg: string;
+        };
+    };
+}
+interface CheckedRequestContents {
+    "json-contents-checker"?: CheckedRequestEntry;
+}
+
+type CheckedRequest = Request & CheckedRequestContents;
 
 export {
     CheckReturn,
@@ -24,5 +40,9 @@ export {
 
     Middleware,
 
-    CheckFunction
+    CheckFunction,
+
+    CheckedRequestEntry,
+    CheckedRequestContents,
+    CheckedRequest
 }
