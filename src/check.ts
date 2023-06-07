cae812744d8a45aaf3b0b54fd03f179949081be2
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-07 13:26:23 +0200

    Renamed some types and added saving to the ExpressJs Request

--- src/check.ts
+++ src/check.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-import { CheckFunction, CheckReturn, ErrorFunction, JsonObject, Middleware, SuccessFunction } from "./types";
+import { CheckFunction, CheckedRequest, CheckedRequestEntry, ErrorFunction, JsonObject, Middleware, SuccessFunction } from "./types";
 
 function chainChecks(
     errorFunction: ErrorFunction,
@@ -14,12 +14,20 @@ function chainChecks(
 }
 
 function chainChecksMiddleware(
-    errorFunction: ErrorFunction,
-    successFunction: SuccessFunction,
     jsonObject: JsonObject,
     ...checks: CheckFunction[]
 ): Middleware {
-    return (req: Request, res: Response, next: NextFunction) => {
+    return (req: CheckedRequest, res: Response, next: NextFunction) => {
+        if (!req["json-contents-checker"]) req["json-contents-checker"] = {error: false, separateChecks: {}};
+
+        const errorFunction = (errorCode: number, errorMsg: string, key: string) => {
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: errorCode, msg: errorMsg, error: true};
+        }
+
+        const successFunction = (successCode: number, successMsg: string, key: string) => {
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: successCode, msg: successMsg, error: false};
+        }
+        
         checks.forEach((value: CheckFunction) => {value(jsonObject, successFunction, errorFunction);});
         next();
     }
