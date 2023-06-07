a6b3bf4ee719f878e3daa6dacaa5dd96a73b180f
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-07 18:21:32 +0200

    Built

--- @types/check.d.ts
+++ @types/check.d.ts
@@ -1,4 +1,6 @@
-import { CheckFunction, ErrorFunction, Middleware, SuccessFunction, ObjectToCheck } from "./types";
+import { Response } from "express";
+import { CheckFunction, CheckedRequest, ErrorFunction, Middleware, SuccessFunction, ObjectToCheck } from "./types";
 declare function chainChecks(errorFunction: ErrorFunction, successFunction: SuccessFunction, objectToCheck: ObjectToCheck, ...checks: CheckFunction[]): () => void;
-declare function chainChecksMiddleware(objectToCheck: ObjectToCheck, ...checks: CheckFunction[]): Middleware;
-export { chainChecks, chainChecksMiddleware };
+declare function chainChecksMiddleware(...checks: CheckFunction[]): Middleware;
+declare function chainChecksMiddlewareCustom(getObjectToCheck?: (req: CheckedRequest, res: Response) => ObjectToCheck, ...checks: CheckFunction[]): Middleware;
+export { chainChecks, chainChecksMiddleware, chainChecksMiddlewareCustom };
--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,7 +1,7 @@
 import { CheckBooleanArgs, CheckStringArgs, CheckNumberArgs, checkBoolean, checkNumber, checkString } from "./bigChecks";
-import { chainChecks, chainChecksMiddleware } from "./check";
+import { chainChecks, chainChecksMiddleware, chainChecksMiddlewareCustom } from "./check";
 import SmallCheck from "./smallCheck";
-import { CheckFunction, CheckReturn, ErrorFunction, ObjectToCheck, Middleware, SuccessFunction, VerySmallCheckFunction, CheckedRequest, CheckedRequestContents, CheckedRequestEntry } from "./types";
-export { checkBoolean, checkNumber, checkString, SmallCheck, chainChecks, chainChecksMiddleware };
-export { ObjectToCheck, CheckReturn, ErrorFunction, SuccessFunction, VerySmallCheckFunction, Middleware, CheckFunction, CheckedRequest, CheckedRequestContents, CheckedRequestEntry };
+import { CheckFunction, CheckReturn, ErrorFunction, ObjectToCheck, Middleware, SuccessFunction, CheckedRequest, CheckedRequestContents, CheckedRequestEntry } from "./types";
+export { checkBoolean, checkNumber, checkString, SmallCheck, chainChecks, chainChecksMiddleware, chainChecksMiddlewareCustom };
+export { ObjectToCheck, CheckReturn, ErrorFunction, SuccessFunction, Middleware, CheckFunction, CheckedRequest, CheckedRequestContents, CheckedRequestEntry };
 export { CheckBooleanArgs, CheckNumberArgs, CheckStringArgs };
