9d571188f9200a8bd4a60c9797a7246a615ea82f
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-10 16:33:36 +0200

    Added tests for the small check function "maxLength"

--- src/tests/small.ts
+++ src/tests/small.ts
@@ -280,6 +280,34 @@ const testVariations: TestVariant[] = [
             .combine()(mockObject, errorFunction, successFunction);
         }
     },
+    {
+        "namePart": "isString.MaxLength",
+        "expectedOutputs": [
+            [true, 0, ""], // "some string"
+            [true, 0, ""], // ""
+            [false, -2, "-2"], // "some very very not too long string"
+            [false, -1, "-1"], // 0
+            [false, -1, "-1"], // 875
+            [false, -1, "-1"], // -98
+            [false, -1, "-1"], // 2.987
+            [false, -1, "-1"], // -98.979
+            [false, -1, "-1"], // true
+            [false, -1, "-1"], // false
+            [false, -1, "-1"], // undefined
+            [false, -1, "-1"], // null
+            [false, -1, "-1"], // Number.NaN
+            [false, -1, "-1"], // Number.MAX_SAFE_INTEGER
+            [false, -1, "-1"], // Number.MIN_SAFE_INTEGER
+            [false, -1, "-1"], // Number.MAX_SAFE_INTEGER + 1
+            [false, -1, "-1"], // Number.MIN_SAFE_INTEGER - 1
+        ],
+        "func": (key: string): CheckReturn => {
+            return new SmallCheck(key)
+            .isString(-1, "-1")
+            .maxLength(-2, "-2", 11)
+            .combine()(mockObject, errorFunction, successFunction);
+        }
+    },
 ];
 
 export default function doChecksSmall() {
