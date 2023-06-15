090727b1d81d98cd2fb3d2a6ec718d187860cd0a
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-15 14:20:30 +0200

    Updated the imports

--- src/stuff for readme/quickOverview.ts
+++ src/stuff for readme/quickOverview.ts
@@ -1,30 +1,35 @@
 // ## Imports
 
 import {
-    SmallCheck, // The class for the small checks
-    checkString, checkBoolean, checkNumber, // The big check functions
+    SmallCheckOnCheck, SmallCheckOnCombine, SmallCheckOnCreation, // Small check classes
+
+    checkBoolean, checkNumber, checkString, // The big check functions
 
     chainChecks, // Function for combining multiple checks into one
-    chainChecksMiddleware, // Function for chaining multiple checks together and using them as ExpressJS middleware
+    chainChecksMiddleware, chainChecksMiddlewareCustom, // Function for chaining multiple checks together and using them as ExpressJS middleware
 
     errorFunctionDebug, errorFunctionDud, // Predefined error functions
     successFunctionDebug, successFunctionDud, // Predefined success functions
 
-    // Types (that you probably won't need):
-
+    // Types:
     ObjectToCheck, // The type for the object that you want to check
 
     CheckBooleanArgs, CheckStringArgs, CheckNumberArgs, // Arguments that the big check functions take
-
-    CheckFunction, // The type for the function that you get returned by calling one of: SmallCheck.combine, checkString, checkBoolean, checkNumber
-    CheckReturn, // what you get returned from calling that function ^
+    
+    CheckFunction, // The type for the function that you get returned by calling one of: checkString, checkBoolean, checkNumber
+    CheckFunctionOnCheck,
+    CheckFunctionOnCombine,
+    CheckFunctionOnCreation,
+    CheckReturn, // What you get returned from calling one of those functions ^
 
     CheckedRequest, // When using middleware in ExpressJS use "req: CheckedRequest" instead of "req: Request"
     CheckedRequestContents, // The attribute in ^ that was set by the middleware
     CheckedRequestEntry, // The value of ^
+
     Middleware, // The type of the function that chainChecksMiddleware returns
 
     ErrorFunction, SuccessFunction // The function types for the error and success functions
+
 } from "@samuel-risner/json-contents-checker";
 
 // ## Error and success functions
