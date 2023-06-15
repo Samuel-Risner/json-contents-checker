08fc268c4abc88e60b3cd84ea390b41cc4c4c710
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-15 15:33:27 +0200

    Delete funcs.ts
    The functions are now included in the module.

--- src/tests/funcs.ts
+++
@@ -1,5 +0,0 @@
-function errorFunction (errorCode: number, errorMsg: string, key: string) {}
-
-function successFunction (successCode: number, successMsg: string, key: string) {}
-
-export { errorFunction, successFunction }
