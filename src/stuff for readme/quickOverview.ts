114edda0de3835f37f9a328f7d9c4402ac21ffd6
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-15 19:07:06 +0200

    Cleaned up

--- src/stuff for readme/quickOverview.ts
+++ src/stuff for readme/quickOverview.ts
@@ -16,11 +16,13 @@ import {
 
     CheckBooleanArgs, CheckStringArgs, CheckNumberArgs, // Arguments that the big check functions take
     
-    CheckFunction, // The type for the function that you get returned by calling one of: checkString, checkBoolean, checkNumber
-    CheckFunctionOnCheck,
-    CheckFunctionOnCombine,
-    CheckFunctionOnCreation,
-    CheckReturn, // What you get returned from calling one of those functions ^
+    CheckFunctionChain, // The type for the function that you get returned by calling one of: "checkString", "checkBoolean", "checkNumber" or "combineChain" on a small check class
+
+    CheckFunctionOnCheck, // The function that you get returned by calling "combine" on "SmallCheckOnCheck"
+    CheckFunctionPreCheck, // The function that you get returned by calling "combine" on "SmallCheckOnCreation" or "SmallCheckOnCombine"
+
+    CheckReturn, // What you get returned from calling one of those two function types ^
+    SmallCheckArgs, // The arguments passed to the small check functions ("CheckFunctionOnCheck") or classes ("combine" on "SmallCheckOnCombine" or the constructor on "SmallCheckOnCreation")
 
     CheckedRequest, // When using middleware in ExpressJS use "req: CheckedRequest" instead of "req: Request"
     CheckedRequestContents, // The attribute in ^ that was set by the middleware
@@ -31,6 +33,7 @@ import {
     ErrorFunction, SuccessFunction // The function types for the error and success functions
 
 } from "@samuel-risner/json-contents-checker";
+
 
 // ## Error and success functions
 
@@ -57,87 +60,87 @@ const mockObject = {
     "key3": 2.6
 }
 
-// ## Use standalone checks
+// // ## Use standalone checks
 
-let someCheck = new SmallCheck("non existent key")
-.isString(-1, "Oh no!")
-.maxLength(-2, "The thing is too long", 5)
-.minLength(-3, "Ups! Too short.", 2)
-.combine();
+// let someCheck = new SmallCheck("non existent key")
+// .isString(-1, "Oh no!")
+// .maxLength(-2, "The thing is too long", 5)
+// .minLength(-3, "Ups! Too short.", 2)
+// .combine();
 
-someCheck(mockObject, errorFunction, successFunction);
-// [false, -1, "Oh no!"]
-// Because the key "non existent key" does not exist on the object.
+// someCheck(mockObject, errorFunction, successFunction);
+// // [false, -1, "Oh no!"]
+// // Because the key "non existent key" does not exist on the object.
 
-someCheck = checkString({
-    key: "key0",
-    invalidChars: "xyz",
-    maxLength: 16
-})
+// someCheck = checkString({
+//     key: "key0",
+//     invalidChars: "xyz",
+//     maxLength: 16
+// })
 
-someCheck(mockObject, errorFunction, successFunction);
-// [true, 0, ""]
+// someCheck(mockObject, errorFunction, successFunction);
+// // [true, 0, ""]
 
-// ## Chain checks together
-let someOtherCheck = chainChecks(errorFunction, successFunction, mockObject,
-new SmallCheck("non existent key")
-.isString(-1, "Oh no!")
-.maxLength(-2, "The thing is too long", 5)
-.minLength(-3, "Ups! Too short.", 2)
-.combine(),
+// // ## Chain checks together
+// let someOtherCheck = chainChecks(errorFunction, successFunction, mockObject,
+// new SmallCheck("non existent key")
+// .isString(-1, "Oh no!")
+// .maxLength(-2, "The thing is too long", 5)
+// .minLength(-3, "Ups! Too short.", 2)
+// .combine(),
 
-new SmallCheck("key2")
-.isString(-1, "Oh no!")
-.maxLength(-2, "The thing is too long", 5)
-.minLength(-3, "Ups! Too short.", 2)
-.combine(),
+// new SmallCheck("key2")
+// .isString(-1, "Oh no!")
+// .maxLength(-2, "The thing is too long", 5)
+// .minLength(-3, "Ups! Too short.", 2)
+// .combine(),
 
-checkBoolean({
-    key: "key1",
-})
-)
+// checkBoolean({
+//     key: "key1",
+// })
+// )
 
-someOtherCheck();
-// Returns nothing, the error and success functions are important.
-// Chaining checks is more useful as ExpressJS middleware.
+// someOtherCheck();
+// // Returns nothing, the error and success functions are important.
+// // Chaining checks is more useful as ExpressJS middleware.
 
-// ## Use chained checks in ExpressJS
+// // ## Use chained checks in ExpressJS
 
-import Express, {Request, Response} from "express";
+// import Express, {Request, Response} from "express";
 
-const app = Express();
+// const app = Express();
 
-app.get(
-    "index",
-    chainChecksMiddleware(
-        new SmallCheck("non existent key")
-        .isString(-1, "Oh no!")
-        .maxLength(-2, "The thing is too long", 5)
-        .minLength(-3, "Ups! Too short.", 2)
-        .combine(),
+// app.get(
+//     "index",
+//     chainChecksMiddleware(
+//         new SmallCheck("non existent key")
+//         .isString(-1, "Oh no!")
+//         .maxLength(-2, "The thing is too long", 5)
+//         .minLength(-3, "Ups! Too short.", 2)
+//         .combine(),
 
-        new SmallCheck("key2")
-        .isString(-1, "Oh no!")
-        .maxLength(-2, "The thing is too long", 5)
-        .minLength(-3, "Ups! Too short.", 2)
-        .combine(),
+//         new SmallCheck("key2")
+//         .isString(-1, "Oh no!")
+//         .maxLength(-2, "The thing is too long", 5)
+//         .minLength(-3, "Ups! Too short.", 2)
+//         .combine(),
 
-        checkBoolean({
-            key: "key1",
-        })
-    ),
-    (req: CheckedRequest, res: Response) => {
-        const checkResult = req["json-contents-checker"] as CheckedRequestEntry;
-        // Check if any check failed:
-        const somethingFailed: boolean = checkResult.error;
+//         checkBoolean({
+//             key: "key1",
+//         })
+//     ),
+//     (req: CheckedRequest, res: Response) => {
+//         const checkResult = req["json-contents-checker"] as CheckedRequestEntry;
+//         // Check if any check failed:
+//         const somethingFailed: boolean = checkResult.error;
 
-        // Check the checks for the individual keys:
-        const check1Results = checkResult.separateChecks["non existent key"];
-        const check1Failed: boolean = check1Results.error;
-        const check1Code: number = check1Results.code;
-        const check1Msg: string = check1Results.msg;
+//         // Check the checks for the individual keys:
+//         const check1Results = checkResult.separateChecks["non existent key"];
+//         const check1Failed: boolean = check1Results.error;
+//         const check1Code: number = check1Results.code;
+//         const check1Msg: string = check1Results.msg;
 
-        // Do whatever you want with the information you now have.
+//         // Do whatever you want with the information you now have.
 
-    }
-);
+//     }
+// );
---
+++ src/stuff for the wiki/imports.ts
@@ -0,0 +1,33 @@
+import {
+    SmallCheckOnCheck, SmallCheckOnCombine, SmallCheckOnCreation, // Small check classes
+
+    checkBoolean, checkNumber, checkString, // The big check functions
+
+    chainChecks, // Function for combining multiple checks into one
+    chainChecksMiddleware, chainChecksMiddlewareCustom, // Function for chaining multiple checks together and using them as ExpressJS middleware
+
+    errorFunctionDebug, errorFunctionDud, // Predefined error functions
+    successFunctionDebug, successFunctionDud, // Predefined success functions
+
+    // Types:
+    ObjectToCheck, // The type for the object that you want to check
+
+    CheckBooleanArgs, CheckStringArgs, CheckNumberArgs, // Arguments that the big check functions take
+    
+    CheckFunctionChain, // The type for the function that you get returned by calling one of: "checkString", "checkBoolean", "checkNumber" or "combineChain" on a small check class
+
+    CheckFunctionOnCheck, // The function that you get returned by calling "combine" on "SmallCheckOnCheck"
+    CheckFunctionPreCheck, // The function that you get returned by calling "combine" on "SmallCheckOnCreation" or "SmallCheckOnCombine"
+
+    CheckReturn, // What you get returned from calling one of those two function types ^
+    SmallCheckArgs, // The arguments passed to the small check functions ("CheckFunctionOnCheck") or classes ("combine" on "SmallCheckOnCombine" or the constructor on "SmallCheckOnCreation")
+
+    CheckedRequest, // When using middleware in ExpressJS use "req: CheckedRequest" instead of "req: Request"
+    CheckedRequestContents, // The attribute in ^ that was set by the middleware
+    CheckedRequestEntry, // The value of ^
+
+    Middleware, // The type of the function that chainChecksMiddleware returns
+
+    ErrorFunction, SuccessFunction // The function types for the error and success functions
+
+} from "@samuel-risner/json-contents-checker";
