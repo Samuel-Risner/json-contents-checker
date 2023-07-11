97d5ba751498f81a5e66cf377140cbdeb76491c9
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-07-11 10:26:44 +0200

    Updated the examples

--- src/stuff for readme/quickOverview.ts
+++ src/stuff for readme/quickOverview.ts
@@ -31,10 +31,15 @@ import {
 
     Middleware, // The type of the function that chainChecksMiddleware returns
 
-    ErrorFunction, SuccessFunction // The function types for the error and success functions
+    ErrorFunction, SuccessFunction, // The function types for the error and success functions
+
+    ChainResult, // What "chainChecks" returns
+    ChainResultKey // Result in ^ for each checked key
 } from "@samuel-risner/json-contents-checker";
 
 // ## Error and success functions
+
+// ### Define your own:
 
 const errorFunction: ErrorFunction = (errorCode: number, errorMsg: string, key: string): void => {
     console.log(`❌ The check on '${key}' failed!`);
@@ -61,101 +66,46 @@ const mockObject: ObjectToCheck = {
 
 // ## Use standalone checks
 
-let someCheck: CheckFunctionOnCheck = new SmallCheckOnCheck()
+// ### Small checks
+
+const someCheck1: CheckFunctionOnCheck = new SmallCheckOnCheck()
     .isString(-1, "The thing that you are trying to check isn't a string.")
     .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combine()
+    .combine();
 
-someCheck({ key: "key0", objectToCheck: mockObject });
+const result1: CheckReturn = someCheck1({ key: "key0", objectToCheck: mockObject });
 
-let someCheck2: CheckFunctionPreCheck = new SmallCheckOnCombine()
+const someCheck2: CheckFunctionPreCheck = new SmallCheckOnCombine()
     .isString(-1, "The thing that you are trying to check isn't a string.")
     .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combine({ key: "key0", objectToCheck: mockObject })
+    .combine({ key: "key0", objectToCheck: mockObject });
 
-someCheck2();
+const result2: CheckReturn = someCheck2();
 
-let someCheck3: CheckFunctionPreCheck = new SmallCheckOnCreation({ key: "key0", objectToCheck: mockObject })
+const someCheck3: CheckFunctionPreCheck = new SmallCheckOnCreation({ key: "key0", objectToCheck: mockObject })
     .isString(-1, "The thing that you are trying to check isn't a string.")
     .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combine()
+    .combine();
 
-someCheck3();
+const result3: CheckReturn = someCheck3();
 
-// ## Chain checks together
-
-let someOtherCheck = chainChecks(errorFunction, successFunction, mockObject,
-    new SmallCheckOnCombine()
-    .isString(-1, "Oh no!")
-    .maxLength(-2, "The thing is too long", 5)
+const otherResult1: CheckReturn = new SmallCheckOnCheck()
+    .isString(-1, "The thing that you are trying to check isn't a string.")
+    .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combineChain("key0"),
+    .evaluate({ key: "key0", objectToCheck: mockObject });
 
-    new SmallCheckOnCreation({ key: "key1", objectToCheck: mockObject })
-    .isString(-1, "Oh no!")
-    .maxLength(-2, "The thing is too long", 5)
+const otherResult2: CheckReturn = new SmallCheckOnCombine()
+    .isString(-1, "The thing that you are trying to check isn't a string.")
+    .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combineChain(),
+    ({ key: "key0", objectToCheck: mockObject });
 
-    new SmallCheckOnCheck()
-    .isString(-1, "Oh no!")
-    .maxLength(-2, "The thing is too long", 5)
+const otherResult3: CheckReturn =  new SmallCheckOnCreation({ key: "key0", objectToCheck: mockObject })
+    .isString(-1, "The thing that you are trying to check isn't a string.")
+    .maxLength(-2, "The string is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
-    .combineChain("key2"),
-
-    checkBoolean({
-        key: "key3"
-    })
-)
-
-someOtherCheck();
-
-// ## Use chained checks in ExpressJS
-
-import Express, {Request, Response} from "express";
-
-const app = Express();
-
-app.get(
-    "index",
-    chainChecksMiddleware(
-        new SmallCheckOnCombine()
-        .isString(-1, "Oh no!")
-        .maxLength(-2, "The thing is too long", 5)
-        .minLength(-3, "Ups! Too short.", 2)
-        .combineChain("key0"),
-
-        new SmallCheckOnCreation({ key: "key1", objectToCheck: mockObject })
-        .isString(-1, "Oh no!")
-        .maxLength(-2, "The thing is too long", 5)
-        .minLength(-3, "Ups! Too short.", 2)
-        .combineChain(),
-
-        new SmallCheckOnCheck()
-        .isString(-1, "Oh no!")
-        .maxLength(-2, "The thing is too long", 5)
-        .minLength(-3, "Ups! Too short.", 2)
-        .combineChain("key2"),
-
-        checkBoolean({
-            key: "key3"
-        })
-    ),
-    (req: CheckedRequest, res: Response) => {
-        const checkResult = req["json-contents-checker"] as CheckedRequestEntry;
-        // Check if any check failed:
-        const somethingFailed: boolean = checkResult.error;
-
-        // Check the checks for the individual keys:
-        const check1Results = checkResult.separateChecks["non existent key"];
-        const check1Failed: boolean = check1Results.error;
-        const check1Code: number = check1Results.code;
-        const check1Msg: string = check1Results.msg;
-
-        // Do whatever you want with the information you now have.
-
-    }
-);
+    .__call__({});
--- src/stuff for the wiki/check chaining/example.ts
+++ src/stuff for the wiki/check chaining/example.ts
@@ -1,29 +1,37 @@
-import { chainChecks, errorFunctionDud, successFunctionDud, SmallCheckOnCheck, SmallCheckOnCombine, SmallCheckOnCreation, checkBoolean } from "@samuel-risner/json-contents-checker";
+import { chainChecks, SmallCheckOnCheck, SmallCheckOnCombine, SmallCheckOnCreation, checkBoolean, ChainResult, ChainResultKey } from "@samuel-risner/json-contents-checker";
 
 import mockObject from "../../mockObject";
 
-const check = chainChecks(errorFunctionDud, successFunctionDud, mockObject,
-    new SmallCheckOnCombine()
+const combinedChecks = chainChecks(mockObject,
+    new SmallCheckOnCheck()
     .isString(-1, "Oh no!")
     .maxLength(-2, "The thing is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
     .combineChain("key0"),
 
-    new SmallCheckOnCreation({ key: "key1", objectToCheck: mockObject })
+    new SmallCheckOnCombine()
+    .isString(-1, "Oh no!")
+    .maxLength(-2, "The thing is too long", 5)
+    .minLength(-3, "Ups! Too short.", 2)
+    .combineChain("key1"),
+
+    new SmallCheckOnCreation({ key: "key2", objectToCheck: mockObject })
     .isString(-1, "Oh no!")
     .maxLength(-2, "The thing is too long", 5)
     .minLength(-3, "Ups! Too short.", 2)
     .combineChain(),
-
-    new SmallCheckOnCheck()
-    .isString(-1, "Oh no!")
-    .maxLength(-2, "The thing is too long", 5)
-    .minLength(-3, "Ups! Too short.", 2)
-    .combineChain("key2"),
 
     checkBoolean({
         key: "key3"
     })
 )
 
-check();
+const combinedResult: ChainResult = combinedChecks();
+
+const anyCheckFailed: boolean = combinedResult.error;
+
+const key0Result: ChainResultKey = combinedResult.separateChecks["key0"];
+
+const checkForKeyFailed: boolean = key0Result.error;
+const codeForKey: number = key0Result.code;
+const messageForKey: string = key0Result.msg;
