4ccbdae3064cb10f3787fd8853df72e6910dece0
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-07-11 09:07:14 +0200

    Fixed a bug
    If a check failed the attribute indicating that any check failed would not change.

--- src/check.ts
+++ src/check.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Response, Request } from "express";
 
 import { CheckFunctionChain, CheckedRequest, CheckedRequestEntry, ErrorFunction, Middleware, SuccessFunction, ObjectToCheck } from "./types";
 
@@ -22,11 +22,12 @@ function chainChecksMiddleware(
         if (!req["json-contents-checker"]) req["json-contents-checker"] = {error: false, separateChecks: {}};
 
         const errorFunction = (errorCode: number, errorMsg: string, key: string) => {
-            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: errorCode, msg: errorMsg, error: true};
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: errorCode, msg: errorMsg, error: true };
+            (req["json-contents-checker"] as CheckedRequestEntry).error = true;
         }
 
         const successFunction = (successCode: number, successMsg: string, key: string) => {
-            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: successCode, msg: successMsg, error: false};
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: successCode, msg: successMsg, error: false };
         }
         
         checks.forEach((value: CheckFunctionChain) => {value(objectToCheck, errorFunction, successFunction);});
@@ -35,7 +36,7 @@ function chainChecksMiddleware(
 }
 
 function chainChecksMiddlewareCustom(
-    getObjectToCheck: (req: CheckedRequest, res: Response) => ObjectToCheck,
+    getObjectToCheck: (req: Request, res: Response) => ObjectToCheck,
     ...checks: CheckFunctionChain[]
 ): Middleware {
     return (req: CheckedRequest, res: Response, next: NextFunction) => {
@@ -44,11 +45,12 @@ function chainChecksMiddlewareCustom(
         if (!req["json-contents-checker"]) req["json-contents-checker"] = {error: false, separateChecks: {}};
 
         const errorFunction = (errorCode: number, errorMsg: string, key: string) => {
-            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: errorCode, msg: errorMsg, error: true};
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: errorCode, msg: errorMsg, error: true };
+            (req["json-contents-checker"] as CheckedRequestEntry).error = true;
         }
 
         const successFunction = (successCode: number, successMsg: string, key: string) => {
-            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: successCode, msg: successMsg, error: false};
+            (req["json-contents-checker"] as CheckedRequestEntry).separateChecks[key] = { code: successCode, msg: successMsg, error: false };
         }
         
         checks.forEach((value: CheckFunctionChain) => {value(objectToCheck, errorFunction, successFunction);});
